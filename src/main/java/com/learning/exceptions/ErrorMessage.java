25ea921921fb8d6dc109053382d174ad79c8ffb2
anmolc09 <anmolc09@gmail.com>
2023-03-12 02:09:46 +0530

    added controller advice

---
+++ src/main/java/com/learning/exceptions/ErrorMessage.java
@@ -0,0 +1,17 @@
+package com.learning.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorMessage {
+    private LocalDateTime timestamp;
+    private String message;
+}
